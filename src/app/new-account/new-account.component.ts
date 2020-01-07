9de37c07bff0aa34f7dfc8246d34864a1231cca2
Paul <paul.roberts@C02XT4NCJGH5.local>
2020-01-07 16:17:19 -0500

    Injecting Services into Services

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -5,7 +5,7 @@ import { AccountsService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -16,6 +16,6 @@ export class NewAccountComponent {
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName,accountStatus);
     // this is better because it stays in the angular echo system
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
