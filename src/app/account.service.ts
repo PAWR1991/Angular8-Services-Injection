ed5b14a2c0a338220f4ca6da770e9c3dbf05b7fc
Paul <paul.roberts@C02XT4NCJGH5.local>
2020-01-07 16:37:12 -0500

    communication with EventEmitter with a service

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,5 +1,5 @@
 import { LoggingService } from "./logging.service";
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
 //you don't add injecable to a service that you want to inject but to the service that is getting inject by another service
 @Injectable()
 export class AccountsService{
@@ -17,6 +17,8 @@ export class AccountsService{
           status: 'unknown'
         }
     ];
+
+    statusUpdated = new EventEmitter<string>();
 
     constructor(private loggingService:LoggingService){}
     
